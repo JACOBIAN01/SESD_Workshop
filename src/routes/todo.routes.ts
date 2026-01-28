7a3c5431a722ca2c95c558bfda5b1609df83dc8a
JACOBIAN01 <subhadeepghorai23@gmail.com>
2026-01-28 23:44:05 +0530

    Update

--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -1,19 +1,24 @@
 import { ToDoController } from "../controllers/todo.controller";
 import { Router } from "express";
 
-export class ToDoRouter {
-  private router = Router();
+class ToDoRouter {
+  router: Router;
   private todoController = new ToDoController();
 
   constructor() {
+    this.router = Router();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
     this.router.get("/", this.todoController.getAllTask);
-
     this.router.post("/", this.todoController.createTaskRoute);
-
     this.router.put("/:id", this.todoController.updateTask);
-
     this.router.delete("/:id", this.todoController.deleteTask);
   }
+
 }
 
+const toDoRouter = new ToDoRouter()
+export default toDoRouter.router
 
